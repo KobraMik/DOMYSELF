4354f7a671f910a0a4f2260fe0c511a0ffab46e6
Mike <slavik433@gmail.com>
2022-01-03 17:08:13 +0300

    add onChange and onKey

--- DOMYSELF/src/App.tsx
+++ DOMYSELF/src/App.tsx
@@ -1,25 +1,28 @@
 import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from "./Todolist";
-
-let [tasks, setTasks] = useState([
-    {id: 1, title: "HTML&CSS", checked: true},
-    {id: 2, title: "JS", checked: true},
-    {id: 3, title: "React", checked: false},
-    ])
-//
-// function addTask () {
-//     let NewTask = {id: 1, title: "HTML&CSS", checked: true}
-//     setTasks([NewTask, ...tasks])
-// }
+import {v1} from "uuid";
 
 function App() {
+    let [tasks, setTasks] = useState([
+        {id: v1(), title: "HTML&CSS", checked: true},
+        {id: v1(), title: "JS", checked: true},
+        {id: v1(), title: "React", checked: false},
+    ])
+
+    function addTask(inputTitle: string) {
+        let NewTask = {id: v1(), title: inputTitle, checked: true}
+        setTasks([NewTask, ...tasks])
+    }
+
+
     return (
         <div className="App">
-           <Todolist title="What to learn"
-                     tasks={tasks}
-           />
-       </div>
+            <Todolist title="What to learn"
+                      tasks={tasks}
+                      addTask={addTask}
+            />
+        </div>
     );
 }
 
--- DOMYSELF/src/Todolist.tsx
+++ DOMYSELF/src/Todolist.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 import './App.css';
 
 type taskType = {
-    id: number;
+    id: string;
     title: string;
     checked: boolean;
 }
@@ -10,24 +10,44 @@ type taskType = {
 type propsType = {
     title: string;
     tasks: Array<taskType>
+    addTask: (inputTitle: string) => void
 }
 
 export function Todolist(props: propsType) {
+    let [inputTitle, setInputTitle] = useState('')
+
+    function addTask() {
+        props.addTask(inputTitle)
+        setInputTitle('')
+    }
+
+    function onChangeHandler(e: React.ChangeEvent<HTMLInputElement>) {
+        setInputTitle(e.currentTarget.value)
+    }
+
     return (
         <div className="App">
             <div>
                 <h3>{props.title}</h3>
                 <div>
-                    <input/>
-                    <button>+</button>
+                    <input onChange={onChangeHandler}
+                           onKeyPress={(e) => {
+                               if (e.key === 'Enter') {
+                                       addTask()
+                               }
+                           }}
+                    />
+                    <button onClick={addTask}
+                    >+
+                    </button>
                 </div>
                 <ul>
                     {props.tasks.map(t => {
                         return <li key={t.id}>
-                                <input type="checkbox"
-                                       checked={t.checked}/>
-                                <span>{t.title}</span>
-                            </li>
+                            <input type="checkbox"
+                                   checked={t.checked}/>
+                            <span>{t.title}</span>
+                        </li>
                     })}
                 </ul>
                 <div>
